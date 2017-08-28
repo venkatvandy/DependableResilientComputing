9f2a279de80677d2897d15123f5e7a5b141d1db7
venkatvandy <venkataramana.nagarajan@vanderbilt.edu>
2017-08-27 22:07:40 -0500

    Push1

---
+++ venkat-assignment0/async_client.py
@@ -0,0 +1,48 @@
+import zmq
+import sys
+import threading
+import time
+from random import randint, random
+
+def tprint(msg):
+    """like print  , but won't get newlines confused with multiple threads"""
+    sys.stdout.write(msg + '\n')
+    sys.stdout.flush()
+
+class ClientTask(threading.Thread):
+    """ClientTask"""
+    def __init__(self, id):
+        self.id = id
+        threading.Thread.__init__ (self)
+
+    def run(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.DEALER)
+	identity = u'worker-%d' % self.id
+        socket.identity = identity.encode('ascii')
+        socket.connect('tcp://localhost:5570')
+        print('Client %s started' % (identity))
+        poll = zmq.Poller()
+        poll.register(socket, zmq.POLLIN)
+        #reqs = 0
+        #while True:
+            #reqs = reqs + 1
+            #print('Req #%d sent..' % (reqs))
+            #socket.send_string(u'request #%d' % (reqs))
+		#socket.send_string(u'request #%d' % (reqs))
+	socket.send_string("Blah")
+	for i in range(5):
+		sockets = dict(poll.poll(1000))
+		if socket in sockets:
+			msg = socket.recv()
+                	tprint('Client %s received: %s' % (identity, msg))
+
+	socket.close()
+        context.term()
+
+def main():
+	client = ClientTask(0)
+	client.start()
+
+if __name__ == "__main__":
+	main()
